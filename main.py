d6f5c2c3bcf35c8dad976ae4470aa03156c3ad45
kharlkhadre <kharl.rparker@gmail.com>
2025-02-04 14:42:58 -0800

    Add player and asteroids with collisions

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -10,14 +14,42 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    
+    player = Player(x, y)
+    asteroidfield = AsteroidField()
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color(0,0,0))
+        screen.fill("black")
+
+        updateable.update(dt)
+
+        for i in asteroids:
+            if i.collision(player):
+                print("Game over!")
+                return
+
+        for i in drawable:
+            i.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
